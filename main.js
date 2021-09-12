ff43ccf622500e0fc52f15e03825ccaf8c8aebf2
kerenhailu <keren4509@gmail.com>
2021-09-12 14:10:47 +0300

    for test

---
+++ main.js
@@ -0,0 +1,114 @@
+// =========================1
+// function nameS (){
+//     for(var i=0;i<5;i++){
+//         console.log(a)
+//     }
+// }
+// var a = "keren"
+// var NAME1=nameS(a)
+
+
+// =========================2
+
+// function newname(keren) {
+//     for (var i = 0; i < 5; i++) {
+//         console.log(keren)
+//     }
+// }
+// var kere = "keren"
+// var funname = newname(kere)
+
+// ===========================3
+
+
+// function two() {
+
+//     var sum = numone + numtwo
+//     console.log(sum)
+// }
+// var numone = 5
+// var numtwo = 6
+// var twoNumber = two(numone, numtwo)
+
+// =========================4
+// function numTWO (one , two){
+//     var sum = one+two
+//     console.log(sum)
+// }
+// var TWO =numTWO(7 , 3) 
+// =========================5
+// function numbers(a,b){
+//     if (a<b){
+//         console.log(b)
+//     }
+//     else{
+//         console.log(a)
+//     }
+// }
+// var numbers = numbers(8+9)
+// =========================6
+
+
+// function numberJASTONE(ONE) {
+//     var a = 0
+//     while (ONE > 0) {
+//         modolo = ONE % 10
+//         ONE =( ONE / 10) - (modolo / 10)
+//         a = a + modolo
+//     }
+//     return a
+// }
+// var JASTONE = numberJASTONE(2468)
+// console.log(JASTONE)
+// =========================9
+// function kelet(marach){
+//     var user = Number(prompt("choose number"))
+//     for (var i = 0; i < marach.length; i++) {
+//         if (user==marach[i]){
+//             marach.splice(i,1)
+//             return i
+//         }
+
+//     }return "לא קיים"
+// }
+// var marach = [1,2,3,4,5]
+// var kelet = kelet(marach)
+// console.log(kelet)
+// =========================10
+// function araay(maraach) {
+//     for (let i = 0; i < maraach.length; i++) {
+//         maraach[i] = maraach[i] * 3
+//     } return maraach
+// }
+// var ma = [2, 4, 6]
+// var a = araay(ma)
+// console.log(a)
+// =========================11
+// function hazarot(array){
+//     for (let i = 0; i < array.length; i++) {
+//         for (let j = i+1; j < array.length; j++) {
+//             while(array[i]==array[j]){
+//                 array.splice(j,1)
+//             }
+//         }        
+//     }return array
+
+// }
+// var arr = [2,2,2,9,9,4,6,8,6,6,69,9,9,9,9,9,9]
+// var newArray = hazarot(arr)
+// console.log(newArray)
+// =========================11
+// עצרת ===================
+
+// var reselt = 1
+// function azert(number){
+//     for (let i = 1; i <= number; i++) {
+//        reselt = reselt*i 
+//     }return reselt
+// }
+// var a = azert(9)
+// console.log(a)
+
+
+
+
